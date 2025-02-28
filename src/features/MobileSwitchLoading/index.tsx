8b959bfd28140afdee47a7c139f9c5829b5cf163
gakiyukr <oriel-cement0n@icloud.com>
2025-02-28 14:19:20 +0800

    Update index.tsx

--- src/features/MobileSwitchLoading/index.tsx
+++ src/features/MobileSwitchLoading/index.tsx
@@ -8,18 +8,18 @@ import { Center, Flexbox } from 'react-layout-kit';
 import { ProductLogo } from '@/components/Branding';
 
 const MobileSwitchLoading: DynamicOptions['loading'] = memo(() => {
-  const { t } = useTranslation('common');
-  return (
-    <Flexbox height={'100%'} style={{ position: 'relative', userSelect: 'none' }} width={'100%'}>
-      <Center flex={1} gap={16} width={'100%'}>
-        <ProductLogo size={48} type={'combine'} />
-        <Center gap={12} horizontal style={{ fontSize: 15, lineHeight: 1.5, opacity: 0.66 }}>
-          <Icon icon={Loader2} size={{ fontSize: 16 }} spin />
-          {t('layoutInitializing')}
-        </Center>
-      </Center>
-    </Flexbox>
-  );
-});
-
+const { t } = useTranslation('common');
+return (
+<Flexbox height="100%" style={{ position: 'relative', userSelect: 'none' }} width="100%">
+<Center flex={1} gap={16} width="100%">
+<ProductLogo size={48} type={'combine'} />
+<Center gap={12} horizontal style={{ fontSize: 15, lineHeight: 1.5, opacity: 0.66 }}>
+<Icon icon={Loader2} size={{ fontSize: 16 }} spin />
+{t('layoutInitializing')}
+</Center>
+</Center>
+</Flexbox>
+   );
+ });
+ 
 export default MobileSwitchLoading;
